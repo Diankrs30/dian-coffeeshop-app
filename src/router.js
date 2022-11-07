d8dc0bd9322d23d26dc56172ecac08deb4bb4ade
Diankrs30 <diankrs30@gmail.com>
2022-11-08 01:16:04 +0700

    add router addproduct, addpromo, editproduct, cart

--- src/router.js
+++ src/router.js
@@ -10,17 +10,41 @@ import ForgotPwd from "./pages/forgot-pwd/Forgot-pwd";
 import ProductDetail from "./pages/product-detail/ProductDetail";
 import History from "./pages/history/History";
 import DetailTransaction from "./pages/detail-transaction/DetailTransaction";
+import AddProduct from "./pages/addProduct/AddProduct";
+import AddPromo from "./pages/addPromo/Addpromo";
+import EditProduct from "./pages/editProduct/EditProduct.js";
+import Cart from "./pages/cart/Cart";
+
+import PrivateElement from "./components/privateElement/PrivateElement";
 
 const router = createBrowserRouter([
   { path: "/", element: <Home />, errorElement: <Error /> },
   { path: "/login", element: <Login /> },
   { path: "/register", element: <Register /> },
   { path: "/profile", element: <Profile /> },
-  { path: "/product", element: <Product /> },
+  {
+    path: "/product",
+    element: (
+      <PrivateElement allowedRoles={["user", "admin"]}>
+        <Product />
+      </PrivateElement>
+    ),
+  },
   { path: "/forgot-password", element: <ForgotPwd /> },
   { path: "/detail-product/:id", element: <ProductDetail /> },
-  { path: "/history", element: <History /> },
+  {
+    path: "/history",
+    element: (
+      <PrivateElement allowedRoles={["user"]}>
+        <History />
+      </PrivateElement>
+    ),
+  },
   { path: "/detail-transaction", element: <DetailTransaction /> },
+  { path: "/add-product", element: <AddProduct /> },
+  { path: "/edit-product", element: <EditProduct /> },
+  { path: "/add-promo", element: <AddPromo /> },
+  { path: "/cart", element: <Cart /> },
 ]);
 
 export default router;

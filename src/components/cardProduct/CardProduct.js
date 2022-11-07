214737a3617d47b87448a852f7c3ffb5eae8199f
Diankrs30 <diankrs30@gmail.com>
2022-11-08 01:20:10 +0700

    fix

--- src/components/cardProduct/CardProduct.js
+++ src/components/cardProduct/CardProduct.js
@@ -4,29 +4,37 @@ import styles from "./CardProduct.module.css";
 import icon from "../../assets/img/icon-food.png";
 import { useNavigate } from "react-router-dom";
 
-function CardProduct({data}) {
-  const navigate = useNavigate()
+function CardProduct({ data }) {
+  const navigate = useNavigate();
   const rupiah = (number) => {
-    return new Intl.NumberFormat("id-ID", {
-      style: "currency",
-      currency: "IDR",
-    }).format(number);
+    return `IDR ${number
+      .toString()
+      .replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1.")}`;
   };
   return (
     <>
-    <div className={styles.wrapper} onClick={()=>navigate(`/detail-product/${data.id}`)}>
-      <div className={`${styles.list} ${styles.flex} ${styles["align-items"]}`}>
-        <div  className={styles.img}>
-          <img className={styles["img-prod"]} src={data.image!==''?"http://localhost:8070" +data.image:icon} alt="" />
-        </div>
-        <div className={`${styles.textProd} ${styles.flex}`}>
-          <div className={styles["text-name"]}>
-            <p>{data.product_name}</p>
-          </div> 
-          <p className={styles["text-price"]}>{rupiah(data.price)}</p>
+      <div
+        className={styles.wrapper}
+        onClick={() => navigate(`/detail-product/${data.id}`)}
+      >
+        <div
+          className={`${styles.list} ${styles.flex} ${styles["align-items"]}`}
+        >
+          <div className={styles.img}>
+            <img
+              className={styles["img-prod"]}
+              src={data.image !== "" ? data.image : icon}
+              alt=""
+            />
+          </div>
+          <div className={`${styles.textProd} ${styles.flex}`}>
+            <div className={styles["text-name"]}>
+              <p>{data.product_name}</p>
+            </div>
+            <p className={styles["text-price"]}>{rupiah(data.price)}</p>
+          </div>
         </div>
       </div>
-    </div>
     </>
   );
 }
